54a0624f85ba5d05adc301fafd5f19bae6031407
ItsNovaFPV <battle.aaron.m@gmail.com>
2024-10-03 23:31:20 -0400

    Update build.js

--- build.js
+++ build.js
@@ -6,16 +6,20 @@ const distDir = path.join(__dirname, 'dist');
 
 // Remove the dist directory if it exists
 if (fs.existsSync(distDir)) {
+  console.log('Removing existing dist directory...');
   fs.rmSync(distDir, { recursive: true, force: true });
 }
 
 // Create the dist directory
+console.log('Creating dist directory...');
 fs.mkdirSync(distDir);
 
 // Copy index.js to the dist directory
+console.log('Copying index.js to dist directory...');
 fs.copyFileSync(path.join(__dirname, 'index.js'), path.join(distDir, 'index.js'));
 
 // Create package.json in the dist directory
+console.log('Creating package.json in dist directory...');
 const packageJson = {
   name: "fnaf",
   version: "1.0.0",
